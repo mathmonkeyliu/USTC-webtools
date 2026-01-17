32c2d9190af33e459dd0bccdfc4a0ea327185c08
mathmonkeyliu <philippinemonkey@outlook.com>
2026-01-17 23:09:54 +0800

    clean up

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import requests.exceptions
-import time
 from jwc import *
 from setting import *
 
-if __name__ == '__main__':
-    test_list = ['MED3019.01', '001664KS.01', '206708.01', 'BIO2002.01', 'ESS1507.01', 'HS1612.03',
-                 'HS1658.01', '001665KS.01', 'PHYS1005A.09', 'PHYS4703.01']
-    
-    # 加载/登录获取Cookies
-    load_cookies(username, password, 1)
-    cookies_list = get_cookies()
-    
-    if not cookies_list:
-        print("未找到可用Cookie")
-        exit()
+def main():
+    drop_classes = ["AI1001A.02"]
+    add_classes = ["BIO3501.01"]
+    multi_session_solution(start_time="11:59:58", stop_time="12:00:05", min_interval=0.1, drop_codes=drop_classes, add_codes=add_classes, login_method='selenium', session_number=10, username=username, password=password)
 
-    session = login_by_cookies(cookies_list[0])
-    
-    try:
-        # 初始化选课状态
-        student_id, turn_id = init_course_select(session)
-        
-        # 获取课程ID
-        assocs = get_class_info(session, student_id, turn_id, test_list, 'id')
-        
-        # 循环抢课
-        while True:
-            for assoc in assocs:
-                # 尝试选课
-                result = add_class(session, student_id, turn_id, assoc)
-                print(f"选课结果: {result}")
-                
-    except requests.exceptions.ConnectionError:
-        print("连接错误")
-        print(f"当前登录状态: {login_check(session)}")
-        time.sleep(10)
-    except Exception as e:
-        print(f"发生错误: {e}")
+if __name__ == '__main__':
+    main()
--- temp.py
+++
@@ -1,8 +0,0 @@
-from jwc.login import *
-from setting import *
-
-import pickle
-with open('cookies.pkl', 'rb') as f:
-    cookies = pickle.load(f)
-print(cookies)
-# login_by_selenium(username, password, save_cookies=True)
